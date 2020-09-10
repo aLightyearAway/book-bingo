ea5bdf1f2e824ffe1ab0b1dd5335683fd15247b5
Andreas Klophmann <andreas.klophmann@gmail.com>
2020-09-10 20:12:59 +0200

    Initial spring boot setup

---
+++ src/main/java/no/alightyearaway/bookbingo/BookBingoApplication.java
@@ -0,0 +1,13 @@
+package no.alightyearaway.bookbingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/alightyearaway/bookbingo/BookBingoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.alightyearaway.bookbingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
